f78d1d8918a942381c6205ba457efd55d8df6206
Anton Verchenko <anton.verch@gmail.com>
2020-05-07 21:46:28 +0300

    Simple implementation of game for 2 people

--- src/client/main.js
+++ src/client/main.js
@@ -4,22 +4,85 @@ let buttons = document.querySelectorAll("button");
 
 let char;
 
+function cleanAllFields() {
+    buttons.forEach(b => b.innerText = "");
+}
+
+function disableAllFields() {
+    buttons.forEach(b => b.disabled = true);
+}
+
+function enableEmptyFields() {
+    buttons.forEach(b => {
+        if (b.innerText === "" || b.innerText === undefined) {
+            b.disabled = false
+        }
+    });
+}
+
 buttons.forEach(b => {
-    b.addEventListener("click", (evt) =>{
+    b.addEventListener("click", (evt) => {
+        if (evt.target.disabled) {
+            console.log(`Button ${evt.target.id} is disabled`);
+            return;
+        }
         evt.preventDefault();
-        b.innerText = char;
+        evt.target.innerText = char;
         socket.emit("cell selected", evt.currentTarget.id, char);
+        disableAllFields();
     });
 });
 
-socket.on("cell selected", (btn, char) => {
-    document.getElementById(btn).innerText = char;
+socket.on("cell selected", (btn, value, currentTurn) => {
+    document.getElementById(btn).innerText = value;
+
+    if (currentTurn === char) {
+        enableEmptyFields();
+    }
 });
 
-socket.on("char", (value) => {
+socket.on("joined", (value, name) => {
     char = value;
+    document.getElementById("chat").innerText = "You've joined the game, you char is " + value;
+    document.getElementById("title").innerText = name;
 });
 
+socket.on("game full", () => {
+    document.getElementById("chat").innerText = "This game already has 2 players";
+})
+
+socket.on("retrieve field", (field, currentTurn) => {
+    Object.keys(field).forEach(key => {
+        document.getElementById(key).innerText = field[key];
+    })
+
+    if (currentTurn === char) {
+        enableEmptyFields();
+    }
+})
+
+socket.on("game end", (winner) => {
+    if (winner === undefined) {
+        document.getElementById("chat").innerText = "This is draw! Game will be restarted in 10 sec.";
+    } else {
+        if (winner === char) {
+            document.getElementById("chat").innerText = "Congratulation! You're the winner!";
+        } else {
+            document.getElementById("chat").innerText = `Player ${winner} has won.`;
+        }
+    }
+
+    disableAllFields();
+})
+
+socket.on("game reset", (currentTurn) => {
+    cleanAllFields();
+    document.getElementById("chat").innerText = "Game was restarted!";
+
+    if (currentTurn === char) {
+        enableEmptyFields();
+    }
+})
 // let url_string = window.location.href;
 // let url = new URL(url_string);
 // let id = url.searchParams.get("id");
--- src/server/server.js
+++ src/server/server.js
@@ -14,35 +14,84 @@ app.get("/", (req, res) => {
     res.sendFile("src/client/index.html");
 });
 
-users = {};
+let users = {};
+let field = {};
+let currentTurn = "X";
+
+const winCombinations = ["012", "345", "678", "036", "147", "258", "148", "246"];
 
 io.on("connection", (socket) => {
-    if (Object.keys(users).length === 0) {
+    if (!Object.values(users).includes("X")) {
         users[socket.id] = "X";
-        socket.emit("char", "X");
-    } else if (Object.keys(users).length === 1) {
+        socket.emit("joined", "X", "Player X");
+    } else if (!Object.values(users).includes("O")) {
         users[socket.id] = "O";
-        socket.emit("char", "O");
+        socket.emit("joined", "O", "Player O");
     } else {
-        socket.disconnect(true)
+        socket.emit("game full");
+        socket.disconnect(true);
         return;
     }
 
+    socket.emit("retrieve field", field, currentTurn)
+
     console.log(`A user connected ${socket.id} with ${users[socket.id]}`);
 
-
     socket.on('disconnect', () => {
-        console.log(`user disconnected ${socket.id}`);
+        console.log(`user disconnected ${socket.id} with ${users[socket.id]}`);
         delete users[socket.id];
-      });
+    });
 
     socket.on("cell selected", (btn, char) => {
         console.log(`Button with id ${btn} was pressed with char ${char}`);
 
-        socket.broadcast.emit("cell selected", btn, char);
+        field[btn] = char;
+        currentTurn = char === "X" ? "O" : "X";
+        socket.broadcast.emit("cell selected", btn, char, currentTurn);
+
+        const winner = findWinner();
+
+        if (winner !== undefined) {
+            io.emit("game end", winner);
+
+            setTimeout(() => {
+                field = {};
+                currentTurn = winner;
+
+                io.emit("game reset", currentTurn);
+            }, 10000)
+        } else if (Object.keys(field).length === 9) {
+            io.emit("game end");
+
+            setTimeout(() => {
+                field = {};
+                currentTurn = "X";
+
+                io.emit("game reset", currentTurn);
+            }, 10000)
+        }
     })
 });
+
+function findWinner() {
+    let winner;
+    for (let c of winCombinations) {
+        let combination = "";
+        for (let n of c.split("")) {
+            if (field["c" + n] !== undefined) {
+                combination += field["c" + n];
+            }
+        }
+
+        if (combination === "XXX") {
+           return  "X";
+        } else if (combination === "OOO") {
+            return "O";
+        }
+    }
+}
 
 http.listen(port, () => {
     console.log(`App is running on port ${port}`);
 });
+
